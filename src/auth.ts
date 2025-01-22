c0981a5fc8cccd0cb7fb5303ec1cea2f78de70cf
Adnan <127187767+adenantok@users.noreply.github.com>
2025-01-22 13:09:53 +0700

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -4,9 +4,10 @@ import { prisma } from "./lib/prisma";
 import Credentials from "next-auth/providers/credentials";
 import { SignInSchema } from "./lib/zod";
 import { compareSync } from "bcrypt-ts";
+import { Adapter } from "next-auth/adapters";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: <Adapter>PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
